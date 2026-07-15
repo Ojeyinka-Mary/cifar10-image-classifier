9e4684d2aae1697c30dd02de2fd956293146fbda
Ojeyinka-Mary <bisiojemary@gmail.com>
2026-07-15 18:16:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,63 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-from tensorflow.keras.models import load_model
+import tensorflow as tf
 
-# Load Model
-model = load_model("cifar10_model.h5")
+# Load TFLite model
+interpreter = tf.lite.Interpreter(model_path="cifar10_model.tflite")
+interpreter.allocate_tensors()
 
-# CIFAR-10 Classes
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
 classes = [
-    "Airplane", "Automobile", "Bird", "Cat", "Deer",
-    "Dog", "Frog", "Horse", "Ship", "Truck"
+    "Airplane",
+    "Automobile",
+    "Bird",
+    "Cat",
+    "Deer",
+    "Dog",
+    "Frog",
+    "Horse",
+    "Ship",
+    "Truck"
 ]
 
 st.title("CIFAR-10 Image Classification System")
 
-st.write("Upload an image and let the AI predict its class.")
-
 uploaded_file = st.file_uploader(
-    "Choose an image",
+    "Upload an image",
     type=["jpg", "jpeg", "png"]
 )
 
 if uploaded_file is not None:
+
     image = Image.open(uploaded_file).convert("RGB")
 
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    st.image(
+        image,
+        caption="Uploaded Image",
+        use_container_width=True
+    )
 
     img = image.resize((32, 32))
-    img = np.array(img) / 255.0
+    img = np.array(img, dtype=np.float32)
+    img = img / 255.0
     img = np.expand_dims(img, axis=0)
 
-    prediction = model.predict(img)
+    interpreter.set_tensor(
+        input_details[0]['index'],
+        img
+    )
+
+    interpreter.invoke()
+
+    prediction = interpreter.get_tensor(
+        output_details[0]['index']
+    )
+
     predicted_class = classes[np.argmax(prediction)]
 
-    st.success(f"Prediction: {predicted_class}")
+    st.success(
+        f"Prediction: {predicted_class}"
+    )
